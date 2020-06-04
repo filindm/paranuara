1a9ddb00977a7fa545d22d3f5b0c152cd9027ab5
Dmitry Filin <filindm@gmail.com>
2020-06-04 21:39:55 +1000

    Add pymongo

--- api/paranuara/app.py
+++ api/paranuara/app.py
@@ -1,9 +1,24 @@
+import os
+from bson import json_util
 from flask import Flask
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ["MONGO_URI"]
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/companies')
+def get_companies():
+    items = mongo.db.companies.find()
+    total = mongo.db.companies.count()
+    return json_util.dumps({
+        'items': list(items),
+        'total': total,
+    })
