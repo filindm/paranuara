5562b76b569e0136267617501017caaa29614a38
Dmitry Filin <filindm@gmail.com>
2020-06-06 11:34:12 +1000

    Add error handling

--- api/paranuara/app.py
+++ api/paranuara/app.py
@@ -1,11 +1,10 @@
 import os
+
 from flask import Flask, jsonify
 from flask_mongoengine import MongoEngine
+from werkzeug.exceptions import HTTPException
+
 from .db import db
-from .models import (
-    Company,
-    Person,
-)
 
 
 app = Flask(__name__)
@@ -14,7 +13,15 @@ app.config['MONGODB_HOST'] = os.environ["MONGODB_HOST"]
 db.init_app(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/health')
+def health():
+    return jsonify(ok=True)
 
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    return jsonify({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    }), e.code
